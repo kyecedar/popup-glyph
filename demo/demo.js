3b8efe31ba4a144de3df4f897b14270e47ea63d4
Kye Cedar <kyedoart@protonmail.com>
2023-11-19 08:36:21 -0800

    initial.

---
+++ demo/demo.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
+const app = express();
+
+const cwd = process.cwd();
+
+app.use('/assets', express.static('./assets'));
+app.use('/lib', express.static('./../lib'));
+
+app.get('/', async (req, res) => {
+	res.status(200);
+	res.sendFile(join(cwd, 'demo.html'));
+});
+
+app.listen(3000, 'localhost', function() {
+	console.log(`View on http://localhost:${this.address().port}`);
+});
